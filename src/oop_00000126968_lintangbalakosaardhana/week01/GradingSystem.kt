943c7cf981a0da7c6bfb4c273ebac591705b4e74
codeee18 <lint4ngboy@gmail.com>
2026-02-04 15:26:08 +0700

    “wwk01: add calculateStatus functionâ

--- src/oop_00000126968_lintangbalakosaardhana/week01/GradingSystem.kt
+++ src/oop_00000126968_lintangbalakosaardhana/week01/GradingSystem.kt
@@ -6,14 +6,9 @@ fun main() {
 
     println("Name: $name , Nilai: $score")
 
-    val grade = when (score) {
-        in 90..100 -> "A"
-        in 80..100 -> "B"
-        in 70..100 -> "C"
-        else -> "D"
-    }
-    println("Grade kamu: $grade")
+    println("Status: ${calculateStatus(score)}")
 }
 
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
 
 
