e491a01fe5af859d158eeb0cae852b65d2844358
codeee18 <lint4ngboy@gmail.com>
2026-02-04 15:58:23 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_00000126968_lintangbalakosaardhana/week01/CircleCalculator.kt
+++ src/oop_00000126968_lintangbalakosaardhana/week01/CircleCalculator.kt
@@ -1,6 +1,6 @@
 package oop_00000126968_lintangbalakosaardhana.week01
 
-fun main(args: Array<String>) {
+fun main() {
     val radius = 7.0
     val pi = 3.14
 
@@ -13,3 +13,4 @@ fun main(args: Array<String>) {
 fun checkSize(area: Double) =
     if (area > 100) "This is a Big Circle"
     else "This is a Small Circle"
+
