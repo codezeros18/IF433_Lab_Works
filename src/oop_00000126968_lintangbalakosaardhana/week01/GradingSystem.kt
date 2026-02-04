5512977d9b2165b36c20dd9f32075e47f1040eba
codeee18 <lint4ngboy@gmail.com>
2026-02-04 15:11:07 +0700

    “week01: refactor to use val and string templates

--- src/oop_00000126968_lintangbalakosaardhana/week01/GradingSystem.kt
+++ src/oop_00000126968_lintangbalakosaardhana/week01/GradingSystem.kt
@@ -4,5 +4,5 @@ fun main() {
     var name: String = "Lintang Balakosa Ardhana"
     var score: Int = 90
 
-    println("Name: " + name + ", Nilai: " + score)
+    println("Name: $name, Nilai: $score")
 }
