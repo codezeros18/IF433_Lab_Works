80efb9134684d84585f5132770a4c7b46c6e4813
codeee18 <lint4ngboy@gmail.com>
2026-02-04 16:17:14 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000126968_lintangbalakosaardhana/week01/GameStore.kt
+++ src/oop_00000126968_lintangbalakosaardhana/week01/GameStore.kt
@@ -1,2 +1,12 @@
 package oop_00000126968_lintangbalakosaardhana.week01
 
+fun main() {
+    val gameTitle = "Cyberpunk 2077"
+    val price = 650000
+
+    val finalPrice = calculateDiscount(price)
+}
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) (price * 0.8).toInt()
+    else (price * 0.9).toInt()
