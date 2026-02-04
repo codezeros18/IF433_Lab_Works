d269797566e8363557f0e0717614e20c96474218
codeee18 <lint4ngboy@gmail.com>
2026-02-04 15:31:29 +0700

    “wwk01: implement null safety checkâ

--- src/oop_00000126968_lintangbalakosaardhana/week01/GradingSystem.kt
+++ src/oop_00000126968_lintangbalakosaardhana/week01/GradingSystem.kt
@@ -7,6 +7,11 @@ fun main() {
     println("Name: $name , Nilai: $score")
 
     println("Status: ${calculateStatus(score)}")
+
+    val studentId: String? = null
+    val idLength = studentId?.length ?: 0
+
+    println("Panjang ID: $idLength")
 }
 
 fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
