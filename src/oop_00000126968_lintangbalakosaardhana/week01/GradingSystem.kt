25cd083331dc84a214ada7c4f68df5afad9f94ab
codeee18 <lint4ngboy@gmail.com>
2026-02-04 15:08:20 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000126968_lintangbalakosaardhana/week01/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_00000126968_lintangbalakosaardhana.week01
+
+fun main() {
+    var name: String = "Lintang Balakosa Ardhana"
+    var score: Int = 90
+
+    println("Name: " + name + ", Nilai: " + score)
+}
