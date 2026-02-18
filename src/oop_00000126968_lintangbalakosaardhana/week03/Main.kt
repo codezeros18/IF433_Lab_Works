0d6e51aafe82cc15bcb3f19c8ac04064d2107124
codeee18 <lint4ngboy@gmail.com>
2026-02-18 15:36:16 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_00000126968_lintangbalakosaardhana/week03/Main.kt
+++ src/oop_00000126968_lintangbalakosaardhana/week03/Main.kt
@@ -1,6 +1,8 @@
 package oop_00000126968_lintangbalakosaardhana.week03
 
 fun main(){
+
+    // Employee Test
     val e = Employee("Budi")
 
     e.salary = -1000
@@ -10,4 +12,16 @@ fun main(){
     e.increasePerformance()
 
     println("Pajak yang harus dibayar : ${e.tax}")
+
+
+    // Weapon Test
+    val w = Weapon("Excalibur")
+
+    w.damage = -50
+    println("Damage: ${w.damage}")
+
+    w.damage = 9999
+    println("Damage: ${w.damage}")
+
+    println("Tier: ${w.tier}")
 }
