723bb0b226730579df924730972295d7c869ddc6
codeee18 <lint4ngboy@gmail.com>
2026-04-15 10:47:27 +0700

    week05: implement polymorphic collection and smart casting

--- src/oop_00000126968_lintangbalakosaardhana/week05/Main.kt
+++ src/oop_00000126968_lintangbalakosaardhana/week05/Main.kt
@@ -1,2 +1,18 @@
 package oop_00000126968_lintangbalakosaardhana.week05
 
+fun main() {
+
+    val pegawaiList = listOf<Pegawai>(
+        Dosen("Pak Budi"),
+        Admin("Bu Siti")
+    )
+
+    for (pegawai in pegawaiList) {
+        pegawai.bekerja()
+
+        when (pegawai) {
+            is Dosen -> pegawai.mengajar()
+            is Admin -> println("${pegawai.name} tidak memiliki tugas mengajar.")
+        }
+    }
+}
