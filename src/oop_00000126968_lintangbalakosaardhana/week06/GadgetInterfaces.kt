177a20c6e834f896d84b2c6dec72dfeca432bfc4
codeee18 <lint4ngboy@gmail.com>
2026-04-15 11:01:18 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000126968_lintangbalakosaardhana/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_00000126968_lintangbalakosaardhana.week06
+
+interface Camera {
+    fun takePhoto() {
+        println("Mengambil foto dengan kamera.")
+    }
+}
+
+interface Phone {
+    fun takePhoto() {
+        println("Mengambil foto dengan kamera HP.")
+    }
+}
