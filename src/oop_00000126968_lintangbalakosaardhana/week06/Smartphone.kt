e2935f763c982f37d9efb325580d3fed76f3e2d3
codeee18 <lint4ngboy@gmail.com>
2026-04-15 11:02:35 +0700

    week06: fix diamond problem using explicit super delegation

--- src/oop_00000126968_lintangbalakosaardhana/week06/Smartphone.kt
+++ src/oop_00000126968_lintangbalakosaardhana/week06/Smartphone.kt
@@ -1,3 +1,9 @@
 package oop_00000126968_lintangbalakosaardhana.week06
 
-class Smartphone : Camera, Phone
+class Smartphone : Camera, Phone {
+
+    override fun takePhoto() {
+        super<Camera>.takePhoto()
+        super<Phone>.takePhoto()
+    }
+}
