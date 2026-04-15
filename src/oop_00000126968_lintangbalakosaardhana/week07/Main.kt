35f89b3a912836998eb9e23ca1e544ab664a246f
codeee18 <lint4ngboy@gmail.com>
2026-04-15 11:23:33 +0700

    week07: test Singleton and Companion Object in main

--- src/oop_00000126968_lintangbalakosaardhana/week07/Main.kt
+++ src/oop_00000126968_lintangbalakosaardhana/week07/Main.kt
@@ -1,5 +1,8 @@
 package oop_00000126968_lintangbalakosaardhana.week07
 
 fun main() {
-    val client = NetworkClient("https://api.umn.ac.id")
+    DatabaseManager.connect()
+
+    val client = NetworkClient.create("https://api.umn.ac.id")
+    println(client.baseUrl)
 }
