49959e55a72771a89b9d424f3a0c0dece3ec6a7e
codeee18 <lint4ngboy@gmail.com>
2026-04-15 12:03:05 +0700

    done fix

--- src/oop_00000126968_lintangbalakosaardhana/week08/UserTest.kt
+++ src/oop_00000126968_lintangbalakosaardhana/week08/UserTest.kt
@@ -2,5 +2,10 @@ package oop_00000126968_lintangbalakosaardhana.week08
 
 fun runMockUnitTest() {
     val data: String? = null
-    println(data!!.length)
+
+    if (data == null) {
+        println("Error: API Invalid: Missing ID")
+    } else {
+        println(data.length)
+    }
 }
