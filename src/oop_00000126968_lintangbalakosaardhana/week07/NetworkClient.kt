d8cd65586c198d6b612f5dc789c5b2410862ca6a
codeee18 <lint4ngboy@gmail.com>
2026-04-15 11:23:09 +0700

    week07: implement companion object as factory in NetworkClient

--- src/oop_00000126968_lintangbalakosaardhana/week07/NetworkClient.kt
+++ src/oop_00000126968_lintangbalakosaardhana/week07/NetworkClient.kt
@@ -2,4 +2,10 @@ package oop_00000126968_lintangbalakosaardhana.week07
 
 class NetworkClient private constructor(
     val baseUrl: String
-)
+) {
+    companion object {
+        fun create(baseUrl: String): NetworkClient {
+            return NetworkClient(baseUrl)
+        }
+    }
+}
