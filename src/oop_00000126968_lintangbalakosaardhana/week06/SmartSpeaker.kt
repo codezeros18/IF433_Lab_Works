22cc2c92cd6590dad4126cfac11ff16ca93230b0
codeee18 <lint4ngboy@gmail.com>
2026-04-15 11:07:04 +0700

    week06: (task) create SmartSpeaker

---
+++ src/oop_00000126968_lintangbalakosaardhana/week06/SmartSpeaker.kt
@@ -0,0 +1,21 @@
+package oop_00000126968_lintangbalakosaardhana.week06
+
+package oop_<nim>_<nama>.week06
+
+class SmartSpeaker(
+    override val id: String,
+    override val name: String
+) : SmartDevice, Switchable {
+
+    override fun turnOn() {
+        println("$name menyala.")
+    }
+
+    override fun turnOff() {
+        println("$name mati.")
+    }
+
+    fun playMusic(song: String) {
+        println("Memutar lagu $song dari Spotify.")
+    }
+}
