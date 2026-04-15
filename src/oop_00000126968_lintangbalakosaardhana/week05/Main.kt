4442be95843a5f8db675b3ad62dc188f2b568453
codeee18 <lint4ngboy@gmail.com>
2026-04-15 10:48:35 +0700

    week05: (task 1) test MathHelper overloading in main

--- src/oop_00000126968_lintangbalakosaardhana/week05/Main.kt
+++ src/oop_00000126968_lintangbalakosaardhana/week05/Main.kt
@@ -15,4 +15,9 @@ fun main() {
             is Admin -> println("${pegawai.name} tidak memiliki tugas mengajar.")
         }
     }
+
+    val math = MathHelper()
+    println("Luas Persegi: ${math.hitungLuas(4)}")
+    println("Luas Persegi Panjang: ${math.hitungLuas(4, 5)}")
+    println("Luas Lingkaran: ${math.hitungLuas(3.0)}")
 }
