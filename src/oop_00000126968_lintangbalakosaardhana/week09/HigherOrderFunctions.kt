cfa62aeaa28de24ae92c1c2ae8f737aecdd538b5
codeee18 <lint4ngboy@gmail.com>
2026-04-22 15:19:58 +0700

    week09: implement filter HOF to extract data

---
+++ src/oop_00000126968_lintangbalakosaardhana/week09/HigherOrderFunctions.kt
@@ -0,0 +1,8 @@
+package oop_00000126968_lintangbalakosaardhana.week09
+
+fun main() {
+    val numbers = listOf(1, 2, 3, 4, 5, 6)
+
+    val evenNumbers = numbers.filter { it % 2 == 0 }
+    println(evenNumbers)
+}
