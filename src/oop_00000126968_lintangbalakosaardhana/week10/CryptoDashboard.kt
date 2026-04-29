4852bd02abe4a671f21dde0acac54d2cc2195480
codeee18 <lint4ngboy@gmail.com>
2026-04-29 15:55:33 +0700

    week10: (task) populate coin repository

--- src/oop_00000126968_lintangbalakosaardhana/week10/CryptoDashboard.kt
+++ src/oop_00000126968_lintangbalakosaardhana/week10/CryptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_00000126968_lintangbalakosaardhana.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 1.5))
+    coinRepo.add(Coin("ETH", 10.0))
+    coinRepo.add(Coin("USDT", 1000.0))
 }
