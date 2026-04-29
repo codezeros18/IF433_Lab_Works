701593f3a636fb929fecf369b51cb44ee25814b8
codeee18 <lint4ngboy@gmail.com>
2026-04-29 15:52:13 +0700

    week10: implement where clause for comparable constraints

--- src/oop_00000126968_lintangbalakosaardhana/week10/NumericalStorage.kt
+++ src/oop_00000126968_lintangbalakosaardhana/week10/NumericalStorage.kt
@@ -3,3 +3,8 @@ package oop_00000126968_lintangbalakosaardhana.week10
 class NumericalStorage<T : Number>(val value: T) {
     fun toDouble(): Double = value.toDouble()
 }
+
+fun <T> compare(a: T, b: T): Boolean
+        where T : Number, T : Comparable<T> {
+    return a > b
+}
