4188ff3ee8c64436b6f5e15e07cb1c19ad36482b
codeee18 <lint4ngboy@gmail.com>
2026-04-29 15:49:17 +0700

    week10: instantiate Box with Int and String types

---
+++ src/oop_00000126968_lintangbalakosaardhana/week10/Main.kt
@@ -0,0 +1,9 @@
+package oop_00000126968_lintangbalakosaardhana.week10
+
+fun main() {
+    val intBox = Box(10)
+    val stringBox = Box("Hello")
+
+    println(intBox.value)
+    println(stringBox.value)
+}
