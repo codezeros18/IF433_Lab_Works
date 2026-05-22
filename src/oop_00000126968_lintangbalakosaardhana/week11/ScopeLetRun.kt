5b9fa014ade3f76f6df4d57d3a69f2704cbcea63
codeee18 <lint4ngboy@gmail.com>
2026-05-22 14:13:39 +0700

    week11: implement let function for null safety

---
+++ src/oop_00000126968_lintangbalakosaardhana/week11/ScopeLetRun.kt
@@ -0,0 +1,10 @@
+package oop_00000126968_lintangbalakosaardhana.week11
+
+fun main() {
+
+    val nullableName: String? = "Tony"
+
+    nullableName?.let {
+        println("Length: ${it.length}")
+    }
+}
