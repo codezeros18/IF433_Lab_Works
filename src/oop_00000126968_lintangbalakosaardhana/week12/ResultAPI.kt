2463f288ef5c0f451bed0e68395e8b8a283802fb
codeee18 <lint4ngboy@gmail.com>
2026-05-22 14:31:54 +0700

    week12: handle Result API success and failure

--- src/oop_00000126968_lintangbalakosaardhana/week12/ResultAPI.kt
+++ src/oop_00000126968_lintangbalakosaardhana/week12/ResultAPI.kt
@@ -1,8 +1,16 @@
 package oop_00000126968_lintangbalakosaardhana.week12
 
 fun divide(a: Int, b: Int): Result<Int> {
+    divide(10, 2)
+        .onSuccess {
+            println("Hasil: $it")
+        }
+        .onFailure {
+            println("Error: ${it.message}")
+        }
 
     return runCatching {
         a / b
     }
+
 }
