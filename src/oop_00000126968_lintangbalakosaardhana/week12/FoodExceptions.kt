e1940f4fc9e35f8bfd33b7bb14350a981921e776
codeee18 <lint4ngboy@gmail.com>
2026-05-22 14:33:22 +0700

    week12: (task) create custom feeder exception classes

---
+++ src/oop_00000126968_lintangbalakosaardhana/week12/FoodExceptions.kt
@@ -0,0 +1,5 @@
+package oop_00000126968_lintangbalakosaardhana.week12
+
+class FoodEmptyException(message: String) : Exception(message)
+
+class FeederJammedException(message: String) : Exception(message)
