8384611c627eebeeaaf5d6de8166642558d092d7
codeee18 <lint4ngboy@gmail.com>
2026-05-22 14:15:15 +0700

    week11: implement apply for object initialization

--- src/oop_00000126968_lintangbalakosaardhana/week11/ScopeConfiguration.kt
+++ src/oop_00000126968_lintangbalakosaardhana/week11/ScopeConfiguration.kt
@@ -4,3 +4,14 @@ class User {
     var name: String = ""
     var age: Int = 0
 }
+
+fun main() {
+
+    val user = User().apply {
+        name = "Lintang"
+        age = 20
+    }
+
+    println(user.name)
+    println(user.age)
+}
