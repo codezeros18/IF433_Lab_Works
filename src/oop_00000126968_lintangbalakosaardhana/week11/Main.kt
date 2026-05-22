f4a5d1c92f2b38ab5ea3d0ef830ce646697bd12c
codeee18 <lint4ngboy@gmail.com>
2026-05-22 14:13:16 +0700

    week11: test extension functions in main

---
+++ src/oop_00000126968_lintangbalakosaardhana/week11/Main.kt
@@ -0,0 +1,9 @@
+package oop_00000126968_lintangbalakosaardhana.week11
+
+fun main() {
+
+    println("Lintang".helloExtension())
+
+    val nullableText: String? = null
+    println(nullableText.safeLength())
+}
