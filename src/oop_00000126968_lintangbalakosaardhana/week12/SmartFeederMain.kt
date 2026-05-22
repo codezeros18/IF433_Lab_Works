74a04b53093b4b19c053c565334219150e33dd30
codeee18 <lint4ngboy@gmail.com>
2026-05-22 14:34:31 +0700

    week12: (task) handle feeder exception using try-catch

--- src/oop_00000126968_lintangbalakosaardhana/week12/SmartFeederMain.kt
+++ src/oop_00000126968_lintangbalakosaardhana/week12/SmartFeederMain.kt
@@ -3,4 +3,21 @@ package oop_00000126968_lintangbalakosaardhana.week12
 fun main() {
 
     var currentKibbleStock = 50
+
+    try {
+
+        currentKibbleStock = dispenseKibble(
+            requestedGram = 80,
+            availableGram = currentKibbleStock,
+            isJammed = false
+        )
+
+    } catch (e: FoodEmptyException) {
+
+        println("Peringatan: ${e.message}")
+
+    } catch (e: FeederJammedException) {
+
+        println("Peringatan: ${e.message}")
+    }
 }
