d5cc8b6048dd2e8d36e6d59ab80537ac3f59bee1
codeee18 <lint4ngboy@gmail.com>
2026-05-22 14:23:21 +0700

    week11: (task) implement device search with let

--- src/oop_00000126968_lintangbalakosaardhana/week11/SmartHomeDashboard.kt
+++ src/oop_00000126968_lintangbalakosaardhana/week11/SmartHomeDashboard.kt
@@ -44,4 +44,11 @@ fun main() {
             10
         )
     )
+    val searchResult = homeDevices.find {
+        it.category == "Camera"
+    }
+
+    searchResult?.let {
+        println(it.diagnose())
+    }
 }
