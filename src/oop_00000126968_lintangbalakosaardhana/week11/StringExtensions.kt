7e87056bc672253ba18bdbe3029f82a55b8967e8
codeee18 <lint4ngboy@gmail.com>
2026-05-22 14:12:22 +0700

    week11: create nullable receiver extension

--- src/oop_00000126968_lintangbalakosaardhana/week11/StringExtensions.kt
+++ src/oop_00000126968_lintangbalakosaardhana/week11/StringExtensions.kt
@@ -3,3 +3,7 @@ package oop_00000126968_lintangbalakosaardhana.week11
 fun String.helloExtension(): String {
     return "Hello, $this"
 }
+
+fun String?.safeLength(): Int {
+    return this?.length ?: 0
+}
