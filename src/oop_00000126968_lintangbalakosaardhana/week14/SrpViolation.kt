7ff2aebc78ff1d739897fb32393453e85d50d579
codeee18 <lint4ngboy@gmail.com>
2026-05-27 23:13:23 +0700

    week13: implement basic writeText function

--- src/oop_00000126968_lintangbalakosaardhana/week14/SrpViolation.kt
+++ src/oop_00000126968_lintangbalakosaardhana/week14/SrpViolation.kt
@@ -1,2 +1,23 @@
 package oop_00000126968_lintangbalakosaardhana.week14
 
+class BadUserManager {
+
+    fun registerUser(name: String) {
+
+        if (name.isBlank()) {
+            println("Nama tidak valid")
+            return
+        }
+
+        println("Save user ke database")
+
+        println("Kirim email ke user")
+    }
+}
+
+fun main() {
+
+    val manager = BadUserManager()
+
+    manager.registerUser("Tony")
+}
