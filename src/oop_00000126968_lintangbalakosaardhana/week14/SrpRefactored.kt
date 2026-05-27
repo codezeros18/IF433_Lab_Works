f96a3a0827d650b4a8ce78f245f460b1717b5fc6
codeee18 <lint4ngboy@gmail.com>
2026-05-27 23:13:56 +0700

    week14: extract EmailService to fix SRP

--- src/oop_00000126968_lintangbalakosaardhana/week14/SrpRefactored.kt
+++ src/oop_00000126968_lintangbalakosaardhana/week14/SrpRefactored.kt
@@ -13,3 +13,26 @@ class UserRepository {
         println("Save $name ke database")
     }
 }
+
+class EmailService {
+
+    fun sendEmail(name: String) {
+        println("Kirim email ke $name")
+    }
+}
+
+fun main() {
+
+    val validator = UserValidator()
+    val repository = UserRepository()
+    val emailService = EmailService()
+
+    val username = "Tony"
+
+    if (validator.validate(username)) {
+
+        repository.save(username)
+
+        emailService.sendEmail(username)
+    }
+}
