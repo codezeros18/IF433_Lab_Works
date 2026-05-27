a9635c589ef9383da9af77cf31f2cefb11783564
codeee18 <lint4ngboy@gmail.com>
2026-05-27 23:14:35 +0700

    week14: create DiscountStrategy interface and implementations

---
+++ src/oop_00000126968_lintangbalakosaardhana/week14/OcpRefactored.kt
@@ -0,0 +1,20 @@
+package oop_00000126968_lintangbalakosaardhana.week14
+
+interface DiscountStrategy {
+
+    fun calculate(price: Double): Double
+}
+
+class VipDiscount : DiscountStrategy {
+
+    override fun calculate(price: Double): Double {
+        return price * 0.8
+    }
+}
+
+class RegularDiscount : DiscountStrategy {
+
+    override fun calculate(price: Double): Double {
+        return price * 0.9
+    }
+}
