e01908abf0648ee4a222336afccf93c0d9458699
codeee18 <lint4ngboy@gmail.com>
2026-05-27 23:17:02 +0700

    week14: segregate device interfaces

---
+++ src/oop_00000126968_lintangbalakosaardhana/week14/IspRefactored.kt
@@ -0,0 +1,16 @@
+package oop_00000126968_lintangbalakosaardhana.week14
+
+interface Printable {
+
+    fun print()
+}
+
+interface Scannable {
+
+    fun scan()
+}
+
+interface Faxable {
+
+    fun fax()
+}
