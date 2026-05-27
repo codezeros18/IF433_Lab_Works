21a635ccea606db26fb0bc67b451f63358eb54e9
codeee18 <lint4ngboy@gmail.com>
2026-05-27 22:55:43 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_00000126968_lintangbalakosaardhana/week13/CryptoTradeLogger.kt
@@ -0,0 +1,9 @@
+package oop_00000126968_lintangbalakosaardhana.week13
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double
+)
